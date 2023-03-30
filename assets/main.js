dc4b950c5682520fe235298239a3bd55d27563bb
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-03-30 02:14:04 +0200

    ciclo while

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+// Consegna:Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+const listaSpesa = [
+     'latte',
+     'cereali',
+     'marmellata',
+     'burro',
+     'succo',
+]
+
+let indice = 0;
+
+let soldatino = true
+
+while( soldatino ){
+     document.writeln(listaSpesa)
+
+     soldatino = false
+
+}
+
